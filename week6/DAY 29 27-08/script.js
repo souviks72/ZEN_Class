fef384d868145a234f5d3d4f433af5a76ff25051
unknown <souvikmandalvit2016@gmail.com>
2020-08-30 00:01:54 +0530

    music player

--- week6/DAY 29 27-08/script.js
+++ week6/DAY 29 27-08/script.js
@@ -7,10 +7,10 @@ var Song = /** @class */ (function () {
     return Song;
 }());
 var Playlist = /** @class */ (function () {
-    function Playlist() {
+    function Playlist(url) {
         var _this = this;
         this.songList = new Array();
-        fetch("https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598", {
+        fetch(url, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-host": "shazam.p.rapidapi.com",
@@ -22,41 +22,86 @@ var Playlist = /** @class */ (function () {
         }).then(function (data) {
             var tracks = data.tracks;
             tracks.forEach(function (track) {
-                console.log(track.hub.actions);
+                //console.log(track.hub.actions);
                 if (track.hub.actions) {
-                    var song = new Song(track.title, track.hub.actions[1].uri, track.images.coveart);
+                    console.log(track.title);
+                    var song = new Song(track.title, track.hub.actions[1].uri, track.images.coverart);
                     var li = document.createElement("li");
-                    li.innerText = song.title;
+                    var songDiv = document.createElement("div");
+                    songDiv.classList.add("songDiv");
+                    var img = document.createElement("img");
+                    img.setAttribute("src", song.imgSrc);
+                    img.style.width = "100px";
+                    img.style.height = "100px";
+                    var h4 = document.createElement("h6");
+                    h4.innerText = song.title;
+                    songDiv.append(img, h4);
+                    //li.append(img,h4);
+                    li.appendChild(songDiv);
                     li.setAttribute("url", song.audioSrc);
                     _this.songList.push(li);
                 }
             });
             var ul = document.querySelector('ul');
+            if (ul.hasChildNodes)
+                ul.innerHTML = "";
             ul.append.apply(ul, _this.songList);
+            var lis = document.querySelectorAll('li');
+            lis.forEach(function (li) {
+                li.addEventListener('click', function (e) {
+                    console.log(e.target);
+                    console.log(li.getAttribute("url"));
+                    var player = document.querySelector('.player');
+                    var aud = document.createElement('audio');
+                    var source = document.createElement('source');
+                    source.src = li.getAttribute("url");
+                    aud.appendChild(source);
+                    aud.autoplay = true;
+                    aud.controls = true;
+                    if (player.hasChildNodes) {
+                        player.removeChild(player.lastChild);
+                    }
+                    player.appendChild(aud);
+                });
+            });
         })["catch"](function (err) {
             console.log(err);
-            //alert("Something went wrong!!"
         });
     }
     return Playlist;
 }());
 var ul = document.querySelector('ul');
-var playList = new Playlist();
-playList.songList.forEach(function (song) {
-    ul.append(song);
+var btns = document.querySelector('.buttons');
+var btn1 = document.createElement("button");
+btn1.setAttribute('url', 'https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598');
+btn1.innerText = "Top Tracks";
+var btn2 = document.createElement("button");
+btn2.setAttribute('url', 'https://shazam.p.rapidapi.com/songs/list-recommendations?locale=en-US&key=484129036');
+btn2.innerText = "Recommendations";
+btns.append(btn1, btn2);
+var buttons = [btn1, btn2];
+btn1.addEventListener('click', function (e) {
+    console.log(btn1.getAttribute('url'));
+    new Playlist(btn1.getAttribute('url'));
 });
-ul.addEventListener('click', function (e) {
-    var s = e.target;
-    var player = document.querySelector('.player');
-    var aud = document.createElement('audio');
-    var source = document.createElement('source');
-    source.src = s.getAttribute("url");
-    aud.appendChild(source);
-    aud.autoplay = true;
-    aud.controls = true;
-    if (player.hasChildNodes) {
-        player.removeChild(player.lastChild);
-    }
-    player.appendChild(aud);
-    console.log(aud);
+btn2.addEventListener('click', function (e) {
+    console.log(btn2.getAttribute('url'));
+    new Playlist(btn2.getAttribute('url'));
 });
+new Playlist('https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598');
+// ul.addEventListener('click',(e: Event)=>{
+//     let s = e.target as HTMLLIElement;
+//     console.log(s);
+// let player = document.querySelector('.player');
+// let aud = document.createElement('audio');
+// let source = document.createElement('source');
+// source.src = li.getAttribute("url");
+// aud.appendChild(source);
+// aud.autoplay = true;
+// aud.controls = true;
+// if(player.hasChildNodes){
+//     player.removeChild(player.lastChild);
+// }
+// player.appendChild(aud);
+//console.log(aud);
+// })
--- week6/DAY 29 27-08/script.ts
+++ week6/DAY 29 27-08/script.ts
@@ -11,9 +11,9 @@ class Song{
 
 class Playlist{
     songList: HTMLLIElement[];
-    constructor(){
+    constructor(url: string){
         this.songList = new Array();
-        fetch("https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598", {
+        fetch(url, {
         	"method": "GET",
         	"headers": {
         		"x-rapidapi-host": "shazam.p.rapidapi.com",
@@ -25,45 +25,104 @@ class Playlist{
         }).then(data =>{
             let {tracks} = data;
             tracks.forEach(track =>{
-                console.log(track.hub.actions);
+                //console.log(track.hub.actions);
                 if(track.hub.actions){
-                    let song = new Song(track.title,track.hub.actions[1].uri,track.images.coveart);
+                    console.log(track.title);
+                    let song = new Song(track.title,track.hub.actions[1].uri,track.images.coverart);
                     let li = document.createElement("li");
-                    li.innerText = song.title;
+
+                    let songDiv = document.createElement("div");
+                    songDiv.classList.add("songDiv");
+                    let img = document.createElement("img");
+                    img.setAttribute("src",song.imgSrc);
+                    
+                    img.style.width = "100px";
+                    img.style.height = "100px";
+                    let h4 = document.createElement("h6");
+                    h4.innerText = song.title;
+                    songDiv.append(img,h4);
+                    //li.append(img,h4);
+                    li.appendChild(songDiv);
                     li.setAttribute("url",song.audioSrc);
                     this.songList.push(li);
-                }
-                
+                } 
             });
             let ul = document.querySelector('ul');
+            if(ul.hasChildNodes)
+                ul.innerHTML="";
             ul.append(...this.songList);
+            let lis = document.querySelectorAll('li');
+
+            lis.forEach(li=>{
+                li.addEventListener('click',(e: Event)=>{
+                    console.log(e.target);
+                    console.log(li.getAttribute("url"));
+                    let player = document.querySelector('.player');
+                    let aud = document.createElement('audio');
+                    let source = document.createElement('source');
+                    source.src = li.getAttribute("url");
+                    aud.appendChild(source);
+                    aud.autoplay = true;
+                    aud.controls = true;
+                    if(player.hasChildNodes){
+                        player.removeChild(player.lastChild);
+                    }
+                    player.appendChild(aud);
+                });
+            })
         })
         .catch(err=>{
             console.log(err);
-            //alert("Something went wrong!!"
         })
     }
 }
 
 let ul = document.querySelector('ul')!;
 
-let playList = new Playlist();
-playList.songList.forEach(song=>{
-    ul.append(song);
+let btns = document.querySelector('.buttons') as HTMLDivElement;
+let btn1 = document.createElement("button");
+
+btn1.setAttribute('url','https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598');
+btn1.innerText = "Top Tracks";
+
+let btn2 = document.createElement("button");
+
+btn2.setAttribute('url','https://shazam.p.rapidapi.com/songs/list-recommendations?locale=en-US&key=484129036');
+btn2.innerText = "Recommendations";
+btns.append(btn1,btn2);
+let buttons = [btn1,btn2];
+
+
+btn1.addEventListener('click',(e: Event)=>{
+    console.log(btn1.getAttribute('url'));
+    new Playlist(btn1.getAttribute('url'));
 });
 
-ul.addEventListener('click',(e: Event)=>{
-    let s = e.target as HTMLLIElement;
-    let player = document.querySelector('.player');
-    let aud = document.createElement('audio');
-    let source = document.createElement('source');
-    source.src = s.getAttribute("url");
-    aud.appendChild(source);
-    aud.autoplay = true;
-    aud.controls = true;
-    if(player.hasChildNodes){
-        player.removeChild(player.lastChild);
-    }
-    player.appendChild(aud);
-    console.log(aud);
-})
+btn2.addEventListener('click',(e: Event)=>{
+    console.log(btn2.getAttribute('url'));
+    new Playlist(btn2.getAttribute('url'));
+});
+
+
+new Playlist('https://shazam.p.rapidapi.com/songs/list-artist-top-tracks?locale=en-US&id=40008598');
+
+
+
+// ul.addEventListener('click',(e: Event)=>{
+//     let s = e.target as HTMLLIElement;
+    
+//     console.log(s);
+    // let player = document.querySelector('.player');
+    // let aud = document.createElement('audio');
+    // let source = document.createElement('source');
+    // source.src = li.getAttribute("url");
+    // aud.appendChild(source);
+    // aud.autoplay = true;
+    // aud.controls = true;
+    // if(player.hasChildNodes){
+    //     player.removeChild(player.lastChild);
+    // }
+    // player.appendChild(aud);
+    //console.log(aud);
+// })
+
